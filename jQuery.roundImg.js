cb37a706894eca0423ce31551e6646a269408eb8
Jenna Smith <jenna@growldesign.co.uk>
2012-11-12 11:48:48 +0000

    adding control for each corner and only applying if border-radius is supported

--- jQuery.roundImg.js
+++ jQuery.roundImg.js
@@ -1,30 +1,48 @@
-/*
- * jQuery roundImg Plugin
- * Example at: http://jsfiddle.net/JJenZz/3gYqj/
- * Copyright (c) 2010 J. Smith (@jjenzz)
+/*!------------------------------------------------------
+ * jQuery roundImg
+ * http://github.com/jjenzz/jQuery.roundImg
+ * ------------------------------------------------------
+ * Copyright (c) 2012 J. Smith (@jjenzz)
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- * $('img.rounded').roundImg(10);
+ * $('img.rounded').roundImg('10px')
+ *
+ * or:
+ *
+ * $('img.rounded').roundImg('10px 50px 10px 50px');
  */
-(function($) {
-    $.fn.roundImg = function(radius) {
-        var img, roundedCss;
-        
-        return this.each(function() {
-            img = $(this);
-            
-            roundedCss = [
-                'display: inline-block;',
-                'background: url(' + this.src + ') no-repeat center;',
-                '-webkit-border-radius: ' + radius + 'px;',
-                '-moz-border-radius: ' + radius + 'px;',
-                'border-radius: ' + radius + 'px;'
-            ];
-            
-            img.wrap('<span class="' + this.className + '" style="' + roundedCss.join(' ') + '" />');
-            img.removeClass(this.className).css({'visibility': 'hidden', 'vertical-align': 'bottom'});
-        });
-    }
-})(jQuery);
+(function($, undefined) {
+  $.fn.roundImg = function(value) {
+    var roundedCss,
+        hasBorderRadius,
+        props = 'borderRadius WebkitBorderRadius MozBorderRadius BorderRadius';
+
+    $.each(props.split(' '), function() {
+      hasBorderRadius = (document.body.style[this] !== undefined);
+      return !hasBorderRadius;
+    });
+
+    return this.each(function() {
+      // do nothing if border
+      // radius is not supported
+      if (!hasBorderRadius) {
+        return false;
+      }
+
+      roundedCss = [
+        'display: inline-block;',
+        'background: url(' + this.src + ') no-repeat center;',
+        '-webkit-border-radius: ' + value + ';',
+        '-moz-border-radius: ' + value + ';',
+        'border-radius: ' + value + ';'
+      ];
+
+      $(this)
+        .wrap('<span class="' + this.className + '" style="' + roundedCss.join(' ') + '"/>')
+        .css({'visibility': 'hidden', 'vertical-align': 'bottom'})
+        .removeClass(this.className);
+    });
+  };
+}(jQuery));
